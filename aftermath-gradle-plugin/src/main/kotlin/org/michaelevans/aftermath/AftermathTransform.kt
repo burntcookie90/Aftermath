f1dd25381b8debe1bea93bed5c9c60b0ebb30964
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2016-03-01 17:57:07 -0600

    begin trying to generate class

--- aftermath-gradle-plugin/src/main/kotlin/org/michaelevans/aftermath/AftermathTransform.kt
+++ aftermath-gradle-plugin/src/main/kotlin/org/michaelevans/aftermath/AftermathTransform.kt
@@ -1,8 +1,11 @@
 package org.michaelevans.aftermath
 
 import com.android.build.api.transform.*
+import com.squareup.javapoet.*
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
+import java.util.*
+import javax.lang.model.element.Modifier
 
 class AftermathTransform(val project: Project) : Transform() {
   override fun getInputTypes() = mutableSetOf(QualifiedContent.DefaultContentType.CLASSES)
@@ -17,35 +20,73 @@ class AftermathTransform(val project: Project) : Transform() {
                          outputProvider: TransformOutputProvider,
                          isIncremental: Boolean) {
     referencedInputs.forEach {
-      val outputDirectory = outputProvider.getContentLocation("aftermath", outputTypes, scopes, Format.DIRECTORY)
+      val outputDirectory = outputProvider.getContentLocation("aftermath",
+                                                              outputTypes,
+                                                              scopes,
+                                                              Format.DIRECTORY)
+      val aftermathClasses = arrayListOf<String>()
 
-      it.directoryInputs.forEach {
-        val inputFile = it.file
-        var changed: FileCollection?
-        if (isIncremental) {
-          changed = project.files()
-          it.changedFiles.forEach {
-            println("file: ${it.component1()}")
-            println("status: ${it.component2()}")
-            if (it.component2() == Status.ADDED || it.component2() == Status.CHANGED) {
-              changed = (changed as FileCollection) + project.files(it.component1())
-            }
-          }
-        }
-        else {
-          changed = project.files()
-          it.file.walkBottomUp()
-                  .filter { it.name.contains("$\$Aftermath") }
-                  .forEach {
-                    changed = (changed as FileCollection) + project.files(it)
-                  }
-        }
+      it.directoryInputs.forEach { findAftermaths(aftermathClasses, isIncremental, it) }
 
-        changed.forEach {
-          
+      val aftermathBuilder = TypeSpec.classBuilder("Aftermath")
+              .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+              .addMethod(generateOnActivityResultMethod(aftermathClasses))
+
+    }
+  }
+
+  private fun findAftermaths(aftermathClasses: ArrayList<String>,
+                             isIncremental: Boolean,
+                             it: DirectoryInput) {
+    var changed: FileCollection?
+    if (isIncremental) {
+      changed = project.files()
+      it.changedFiles.forEach {
+        println("file: ${it.component1()}")
+        println("status: ${it.component2()}")
+        if (it.component2() == Status.ADDED || it.component2() == Status.CHANGED) {
+          changed = (changed as FileCollection) + project.files(it.component1())
         }
       }
     }
+    else {
+      changed = project.files()
+      it.file.walkBottomUp()
+              .filter { it.name.contains("$\$Aftermath") }
+              .forEach { changed = (changed as FileCollection) + project.files(it) }
+    }
+
+    changed?.forEach {
+      aftermathClasses.add(it.name)
+    }
   }
+
+  private fun generateOnActivityResultMethod(aftermathClasses: List<String>): MethodSpec {
+    val intentClass = ClassName.get("android.content", "Intent")
+    val methodBuilder = MethodSpec.methodBuilder("onActivityResult")
+            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+            .returns(TypeName.VOID)
+            .addParameter(ParameterSpec.builder(Object::class.java,
+                                                "target",
+                                                Modifier.FINAL).build())
+            .addParameter(ParameterSpec.builder(TypeName.INT,
+                                                "requestCode",
+                                                Modifier.FINAL)
+                                  .build())
+            .addParameter(ParameterSpec.builder(TypeName.INT,
+                                                "resultCode",
+                                                Modifier.FINAL)
+                                  .build())
+            .addParameter(ParameterSpec.builder(intentClass,
+                                                "data",
+                                                Modifier.FINAL)
+                                  .build())
+
+    aftermathClasses.forEach {
+    }
+
+    return methodBuilder.build()
+  }
+
 }
 
